265e40de7d48eeb284246a734f0aa69bc9d8eba6
smacko <samuel.macko.sm@gmail.com>
2020-04-05 16:29:02 +0200

    Additions to repository handling

--- data_gathering/dataset.py
+++ data_gathering/dataset.py
@@ -1,6 +1,6 @@
 
 import csv
-from typing import List, Any
+from typing import Any, List, Optional
 
 from .repository_data import RepositoryData
 
@@ -9,25 +9,39 @@ import pandas as pd
 
 class Dataset:
 
-    def __init__(self, links_file_name: str) -> None:
+    def __init__(
+            self, links_file_name: str, headers: List[str]
+    ) -> None:
         self.links_file_name: str = links_file_name
-        self.data: List[List[Any]] = []
-        # RepositoryData(url=)
-        rd = RepositoryData(url='oVirt/ovirt-web-ui')
-        # print(rd.releases_avg_download_count())
-        print(rd.code_length)
+        self.headers = headers
+        self.data_raw: Optional[List[List[Any]]] = None
+        self.data_df: Optional[pd.DataFrame] = None
 
-    # def compile_data(self) -> None:
-    #     with open(self.links_file_name, 'r') as file:
-    #         for link in file.readlines():
-    #             rd = RepositoryData(url=link, git=self.git)
-    #             self.data.append(rd.export_line())
-    #
-    # def get_df(self) -> pd.DataFrame:
-    #     return pd.DataFrame(self.data, columns=self.headers)
-    #
-    # def export_csv(self, file_name: str = None) -> None:
-    #     with open(file_name or 'dataset.csv', 'w') as file:
-    #         writer = csv.writer(file, delimeter=',')
-    #         writer.writerow(self.headers)
-    #         writer.writerows(self.data)
+    def compile_data(
+            self, size_limit: int = 10, age_limit: int = 10,
+            contributors_limit: int = 5, pull_last_age_limit: int = 5
+    ) -> None:
+        with open(self.links_file_name, 'r') as file:
+            for link in file.readlines():
+                rd = RepositoryData(url=link)
+                if (
+                        rd.size > size_limit and
+                        rd.age > age_limit and
+                        rd.contributors_count > contributors_limit and
+                        rd.pull_last_age > pull_last_age_limit
+                ):
+                    dataset_line = []
+                    # TODO append all the relevant data to the dataset_line
+
+                    self.data_raw.append(dataset_line)
+
+    def create_df(self) -> None:
+        if self.data_raw is None:
+            raise ValueError('Dataset is empty')
+        self.data_df = pd.DataFrame(self.data_raw, columns=self.headers)
+
+    def export_csv(self, file_name: str = None) -> None:
+        with open(file_name or 'dataset.csv', 'w') as file:
+            writer = csv.writer(file, delimeter=',')
+            writer.writerow(self.headers)
+            writer.writerows(self.data_raw)
--- data_gathering/repository_data.py
+++ data_gathering/repository_data.py
@@ -1,4 +1,6 @@
 
+from .enums import Attributes
+
 from datetime import datetime
 import os
 from typing import Any, List, Optional
@@ -6,6 +8,8 @@ from typing import Any, List, Optional
 from dateutil.relativedelta import relativedelta
 from github import Github
 from github.GitRelease import GitRelease
+from github.Issue import Issue
+from github.PullRequest import PullRequest
 from github.Repository import Repository
 from github.StatsContributor import StatsContributor
 
@@ -17,60 +21,114 @@ class RepositoryData:
     def __init__(self, url: str) -> None:
         self._git: Github = Github(login_or_token=_GITHUB_ACCESS_TOKEN)
         self._repo: Repository = self._git.get_repo(full_name_or_id=url)
-        self.create_date: datetime.date = self._repo.created_at.date()
-        self.age: int = self.get_age(date=self.create_date)
-        self.contributors_raw: List[StatsContributor] = self._repo.get_stats_contributors()
-        self.num_contributors: int = len(self.contributors_raw)
-        self.releases_raw: List[GitRelease] = list(self._repo.get_releases())
-        self.num_releases: int = len(self.releases_raw)
-        self.last_release_age: int = self.get_age(
-            date=self.releases_raw[0].created_at
+        self._issues_raw: List[Issue] = list(self._repo.get_issues())
+        self._contributors_raw: List[StatsContributor] = self._repo.get_stats_contributors()
+        self._releases_raw: List[GitRelease] = list(self._repo.get_releases())
+        self._pulls_raw: List[PullRequest] = list(
+            self._repo.get_pulls(state='closed')
         )
-        self.last_release_downloads: int = self.release_download_count(
-            release=self.releases_raw[0]
+        self._create_date: datetime.date = self._repo.created_at.date()
+        self.age: int = self.get_age(date=self._create_date)
+        self.size: int = self._repo.size
+        self.pull_last_age: int = self.get_age(
+            date=self._pulls_raw[0].created_at
         )
-        self.code_length: int = self.get_code_length()
+        self.contributors_count: int = len(self._contributors_raw)
 
-    def relevant_cut(
-            self, attribute_name: str,
-            months_begin: int = 0,
-            months_end: Optional[int] = None
-    ) -> List[Any]:
-        months_begin = self.normalize_months(months=months_begin)
-        months_end = self.normalize_months(months=months_end)
-        if attribute_name == 'release':
-            attribute = self.releases_raw
-        else:
-            raise ValueError('Wrong attribute name')
-        present_date = datetime.now().date()
-        threshold_date = present_date - relativedelta(months=months_end)
-        for i, release in enumerate(attribute):
-            # TODO published_at is specific to the release, needs to be changed to something general
-            if release.published_at.date() < threshold_date:
-                return attribute[months_begin:i]
-        return attribute[months_begin:]
-
-    @staticmethod
-    def get_age(date: datetime.date) -> int:
-        present_date = datetime.now().date()
-        delta = relativedelta(present_date, date)
-        return delta.years * 12 + delta.months
-
-    def normalize_months(self, months: Optional[int]) -> int:
-        if months < 0:
-            return 0
-        if (months is None) or (months > self.age):
-            months = self.age
-        return months
-
-    def releases_frequency(
+    def issues_ration(
             self, months_begin: int = 0, months_end: Optional[int] = None
     ) -> float:
         relevant_cut = self.relevant_cut(
-            attribute_name='release', months_begin=months_begin,
+            attribute=Attributes.ISSUES, months_begin=months_begin,
             months_end=months_end
         )
-        return len(relevant_cut) / (months_end - months_begin)
+        opened_count = closed_count = 0
+        for issue in relevant_cut:
+            opened_count += issue.state == 'opened'
+            closed_count += issue.state == 'closed'
+        return opened_count / closed_count
+
+    def issues_avg_close_time(
+            self, months_begin: int = 0, months_end: Optional[int] = None
+    ) -> float:
+        relevant_cut = self.relevant_cut(
+            attribute=Attributes.ISSUES, months_begin=months_begin,
+            months_end=months_end
+        )
+        total_time_to_close = 0
+        for issue in relevant_cut:
+            delta = issue.closed_at - issue.created_at
+            total_time_to_close += delta.total_seconds() / 3600
+        return total_time_to_close / len(relevant_cut)
+
+    def issues_avg_close_count(
+            self, months_begin: int = 0, months_end: Optional[int] = None
+    ) -> float:
+        relevant_cut = self.relevant_cut(
+            attribute=Attributes.ISSUES, months_begin=months_begin,
+            months_end=months_end
+        )
+        count = 0
+        for issue in relevant_cut:
+            count += issue.state == 'closed'
+        return count / len(relevant_cut)
+
+    def pulls_avg_close_time(
+            self, months_begin: int = 0, months_end: Optional[int] = None
+    ) -> float:
+        relevant_cut = self.relevant_cut(
+            attribute=Attributes.PULLS, months_begin=months_begin,
+            months_end=months_end
+        )
+        total_time_to_close = 0
+        for pull in relevant_cut:
+            delta = pull.closed_at - pull.created_at
+            total_time_to_close += delta.total_seconds() / 3600
+        return total_time_to_close / len(relevant_cut)
+
+    def pulls_avg_close_count(
+            self, months_begin: int = 0, months_end: Optional[int] = None
+    ) -> float:
+        relevant_cut = self.relevant_cut(
+            attribute=Attributes.PULLS, months_begin=months_begin,
+            months_end=months_end
+        )
+        count = 0
+        for pull in relevant_cut:
+            count += pull.state == 'closed'
+        return count / len(relevant_cut)
+
+    def pulls_avg_addition(
+            self, months_begin: int = 0, months_end: Optional[int] = None
+    ) -> float:
+        relevant_cut = self.relevant_cut(
+            attribute=Attributes.PULLS, months_begin=months_begin,
+            months_end=months_end
+        )
+        total_addition = 0.0
+        for pull in relevant_cut:
+            total_addition += pull.additions - pull.deletions
+        return total_addition / len(relevant_cut)
+
+    def attribute_count(
+            self, attribute_name: str, months_begin: int = 0,
+            months_end: Optional[int] = None
+    ) -> int:
+        relevant_cut = self.relevant_cut(
+            attribute=attribute_name, months_begin=months_begin,
+            months_end=months_end
+        )
+        return len(relevant_cut)
+
+    def attribute_frequency(
+            self, attribute_name: str, months_begin: int = 0,
+            months_end: Optional[int] = None
+    ) -> float:
+        count = self.attribute_count(
+            attribute_name=attribute_name, months_begin=months_begin,
+            months_end=months_end
+        )
+        return count / (months_end - months_begin)
 
     @staticmethod
     def release_download_count(release: GitRelease) -> int:
@@ -83,7 +141,7 @@ class RepositoryData:
             self, months_begin: int = 0, months_end: Optional[int] = None
     ) -> float:
         relevant_cut = self.relevant_cut(
-            attribute_name='release', months_begin=months_begin,
+            attribute=Attributes.RELEASES, months_begin=months_begin,
             months_end=months_end
         )
         combined_downloads = 0
@@ -91,17 +149,73 @@ class RepositoryData:
             combined_downloads += self.release_download_count(release=release)
         return combined_downloads / len(relevant_cut)
 
+    def release_last_age(self) -> int:
+        return self.get_age(
+            date=self._repo.get_latest_release().created_at
+        )
+
+    def release_last_downloads(self) -> int:
+        return self.release_download_count(
+            release=self._repo.get_latest_release()
+        )
+
+    def relevant_cut(
+            self, attribute: Attributes,
+            months_begin: int = 0,
+            months_end: Optional[int] = None
+    ) -> List[Any]:
+        """
+        Get only relevant objects (e.g. pulls, issues, releases)
+
+        Args:
+            attribute {Attribute} -- type of attribute (e.g. pulls, issues, releases)
+            months_begin {int} -- how many months back to start the slice
+            months_end {int} -- how many months back to end the slice
+
+        Returns:
+            List[Any] -- slice of relevant objects
+        """
+        months_begin = self.normalize_months(months=months_begin)
+        months_end = self.normalize_months(months=months_end)
+        present_date = datetime.now().date()
+        threshold_date = present_date - relativedelta(months=months_end)
+        for i, att in enumerate(attribute[0]):
+            if getattr(att, attribute[1]).date() < threshold_date:
+                return attribute[months_begin:i]
+        return attribute[months_begin:]
+
+    @staticmethod
+    def get_age(date: datetime.date) -> int:
+        """
+        Convert date in datetime.date to age in months
+
+        Args:
+            date {datetime.date} -- date
+
+        Returns:
+            int -- age in months
+        """
+        present_date = datetime.now().date()
+        delta = relativedelta(present_date, date)
+        return delta.years * 12 + delta.months
+
+    def normalize_months(self, months: Optional[int]) -> int:
+        if (months is None) or (months > self.age):
+            return self.age
+        elif months < 0:
+            return 0
+
     def contributors_avg_activity(
             self, num_of_weeks: Optional[int] = None
     ) -> float:
-        if self.num_contributors == 0:
+        if self.contributors_count == 0:
             raise ValueError('Repository has no contributors')
         activity = 0
-        for contributor in self.contributors_raw:
+        for contributor in self._contributors_raw:
             activity = self.contributor_avg_activity(
                 contributor=contributor, num_of_weeks=num_of_weeks
             )
-        return activity / self.num_contributors
+        return activity / self.contributors_count
 
     @staticmethod
     def contributor_avg_activity(
@@ -119,9 +233,17 @@ class RepositoryData:
             num_of_weeks = len(contributor.weeks)
         return (additions + deletions) / num_of_weeks
 
-    def get_code_length(self) -> int:
-        # problematic languages?: shell, makefile, m4, applescript, dockerfile, ...
+    def code_length(self) -> int:
         length = 0
         for _, lang_length in self._repo.get_languages().items():
             length += lang_length
         return length
+
+    def watchers_count(self) -> int:
+        return self._repo.watchers_count
+
+    def forks_count(self) -> int:
+        return self._repo.forks_count
+
+    def stargazers_count(self) -> int:
+        return self._repo.stargazers_count
